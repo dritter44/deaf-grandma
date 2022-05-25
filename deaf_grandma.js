e0412b3a599990ae7d170a06d01223169dd5becf
dritter44 <dreither44@gmail.com>
2022-05-25 18:12:29 -0500

    deaf grandma first submissions

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,39 @@
 function deafGrandma() {
+    let countGB = 0
+    while(countGB == 0) {
+        let userInput = prompt("HEY KID!","Hello Grandma!")
+       
+        if(userInput === "GOODBYE!"){ //case to break the first while loop
+            countGB++
+            alert("LEAVING SO SOON?")
+            
+        }else if (userInput === ""){
+            alert("WHAT?")
+        
+        }else if(userInput === userInput.toUpperCase() && userInput !== "GOODBYE!"){
+            alert("NO, NOT SINCE 1946!")
+        }else {
+            alert("SPEAK UP KID!")
+        }
 
+    }
+    while(countGB == 1) {
+        let userInput = prompt("HEY KID!","Hello Grandma!")
+       
+        if(userInput === "GOODBYE!"){ //case to break the 2nd while loop
+            alert("LATER SKATER!")
+        return
+            
+        }else if (userInput === ""){
+            alert("WHAT?")
+        
+        }else if(userInput === userInput.toUpperCase() && userInput !== "GOODBYE!"){
+            alert("NO, NOT SINCE 1946!")
+        }else {
+            alert("SPEAK UP KID!")
+        }
+
+    }
+    
 }
-
 deafGrandma();
---
+++ test.js
@@ -0,0 +1,7 @@
+let x = 3
+while(x>1){
+    let y = 2
+    console.log(x,y)
+    y--
+    x--
+}
