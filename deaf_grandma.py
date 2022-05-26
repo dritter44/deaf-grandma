4466cab17cac38fd97a7a2788685fc234d800b04
dritter44 <dreither44@gmail.com>
2022-05-25 21:25:43 -0500

    first commit for deaf grandma in python

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,28 @@
 def deaf_grandma():
-
+    count_GB = 0
+    while count_GB == 0:
+        # need to find a way to prompt user
+        user_input = input('HEY KID!')
+        if user_input == 'GOODBYE!':
+            count_GB += 1
+            print('LEAVING SO SOON!')
+        elif user_input == '':
+            print("WHAT?!")
+        elif user_input == user_input.upper() and user_input != 'GOODBYE!':
+            print("NO! NOT SINCE 1946!")
+        else:
+            print("SPEAK UP ")
+    while count_GB == 1:
+        user_input = input('HEY KID!')
+        if user_input == 'GOODBYE!':
+            print('LATER SKATER!')
+            return
+        elif user_input == '':
+            print("WHAT?!")
+        elif user_input == user_input.upper() and user_input != 'GOODBYE!':
+            print("NO! NOT SINCE 1946!")
+        else:
+            print("SPEAK UP ")
 
 
 deaf_grandma()
